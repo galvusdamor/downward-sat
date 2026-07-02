1d819eec91a827b84f8b5f77d817879b946504aa
Galvus Damor <galvusdamor@gmail.com>
2026-07-02 10:48:08 +0200

    Determine current mem usage

--- src/search/utils/system.h
+++ src/search/utils/system.h
@@ -69,6 +69,7 @@ NO_RETURN extern void exit_with(ExitCode returncode);
 NO_RETURN extern void exit_with_reentrant(ExitCode returncode);
 
 int get_peak_memory_in_kb();
+int get_current_memory_in_kb();
 const char *get_exit_code_message_reentrant(ExitCode exitcode);
 bool is_exit_code_error_reentrant(ExitCode exitcode);
 void register_event_handlers();
